f7b0dfa4b8179b0466a348c2fdea6d59953d8793
Tanishq Sharma <ponasanthem@gmail.com>
2025-08-22 23:47:41 +0530

    Initial Commit: Added Introductory Python Code

---
+++ 100 days of code/Python Learnings/Intro.py
@@ -0,0 +1,24 @@
+
+#? Introduction to Python:
+
+#* Python Print function:
+
+print("Hello! World") # printing strings
+print(30) # printing integers
+print(3.5) # printing floats
+print(True) # printing boolean
+
+
+#* Python Data types:
+
+a = "God!"
+print(a, type(a))
+
+b = 100
+print(b, type(b))
+
+c = 3.13
+print(c, type(c))
+
+d = False
+print(d, type(d))
